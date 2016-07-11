5fcf9f197fe5b593806894c71882e0390d7142eb
YisroelArnson <yisroelrnsn@gmail.com>
2016-07-11 01:13:27 -0400

    abbas version

--- assests/js/home-page.js
+++ assests/js/home-page.js
@@ -1,2 +1,34 @@
 $(function() {
+  mobileNav(200);
+  checkContent();
 });
+
+
+
+function mobileNav(duration) {
+  var $mobileContainer = $('.mobile-nav');
+  var $x = $('.x');
+
+  $x.click(function() {
+    $mobileContainer.toggle(duration);
+  });
+}
+
+
+
+
+
+function checkContent() {
+  $button = $('.button');
+
+  $button.on('click', function() {
+    var $this = $(this);
+    var $secondClass = $this.attr("class").split(' ');
+    var $thisContent = $secondClass[2] + '-' + $secondClass[3];
+
+    $button.css('background-color', 'transparent');
+    $this.css('background-color', '#2980b9');
+    $('.content').slideUp();
+    $('.' + $thisContent).slideDown();
+  });
+}
