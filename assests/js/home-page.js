28c7932ce6c8a9680bdabe474a828f05ae6da779
YisroelArnson <yisroelrnsn@gmail.com>
2016-07-21 03:10:07 -0400

    new slider works except one bug, post on stackoverflow

--- assests/js/home-page.js
+++ assests/js/home-page.js
@@ -6,20 +6,20 @@
 //   mobileScript();
 // });
 //
-//
-//
-// function mobileNav() {
-//   var $linkContainer = $('.link-container');
-//   var $x = $('.x');
-//
-//   $x.click(function() {
-//     $linkContainer.toggleClass('nav-open');
-//   });
-// }
-//
-//
-//
-//
+
+
+function mobileNav() {
+  var $linkContainer = $('.link-container');
+  var $x = $('.x');
+
+  $x.click(function() {
+    $linkContainer.toggleClass('nav-open');
+  });
+}
+
+
+
+
 // function mainSlider() {
 //   var stop1 = false;
 //   var stop2 = false;
@@ -139,128 +139,169 @@
 //     });
 //   }
 // }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// // function checkContent() {
-// //     var $buttons1 = $('.whitespace-1 .button');
-// //     var $content1 = $('.whitespace-1 .content');
-// //     var $buttons2 = $('.whitespace-2 .button');
-// //     var $content2 = $('.whitespace-2 .content');
-// //
-// //     $buttons1.on('click', function() {
-// //       var $this = $(this);
-// //       changeButton($this, $buttons1, $content1, '#f6f6f6', '#ddd');
-// //     });
-// //
-// //     $buttons2.on('click', function() {
-// //       var $this = $(this);
-// //       changeButton($this, $buttons2, $content2, '#003F83', '#1A5DA1');
-// //     });
-// //
-// //
-// //     function changeButton($this, button, content, backgroundColor1, backgroundColor2) {
-// //       var $secondClass = $this.attr("class").split(' ');
-// //       var $thisContent = $secondClass[2] + '-' + $secondClass[3];
-// //
-// //       button.css('background-color', backgroundColor2);
-// //       $this.css('background-color', backgroundColor1);
-// //
-// //       content.css('display', 'none');
-// //       $('.' + $thisContent).fadeIn(800);
-// //     }
-// //
-// // }
-//
-//
-// function checkContent() {
-//     var $buttons = '.whitespace-container .button';
-//     var $content = '.whitespace-container .content';
-//
-//     $($buttons).on('click', function() {
-//       var $this = $(this);
-//       var $secondClass = $this.attr("class").split(' ');
-//       var $thisContent = $secondClass[2] + '-' + $secondClass[3];
-//       var $parent = $this.closest('.whitespace').attr('class').split(' ')[1]
-//
-//       $('.' + $parent + ' ' + $buttons).css('background-color', '#1A5DA1');
-//       $this.css('background-color', '#003F83');
-//
-//       $('.' + $parent + ' ' + $content).css('display', 'none');
-//       $('.' + $thisContent).fadeIn(800);
-//     });
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// function catchPhrase() {
-//   var $allPhrase = $('.ppc-catch-phrase');
-//   var $phrase = '.catch-phrase-';
-//
-//   var catchCounter = 2;
-//   var animationSpeed = 800;
-//   setInterval(function() {
-//     $allPhrase.fadeOut();
-//     $($phrase + catchCounter).fadeIn(animationSpeed);
-//     ++catchCounter
-//     if (catchCounter === $allPhrase.length + 1) {
-//       catchCounter = 1;
-//     }
-//   }, 6000);
-//
-//
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// function smoothScroll() {
-//   $("a").on('click', function(event) {
-//
-//     if (this.hash !== "#") {
-//       event.preventDefault();
-//
-//       var hash = this.hash;
-//
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-//
-//         window.location.hash = hash;
-//       });
-//     }
-//   });
-// }
+
+
+
+
+
+
+
+function color1() {
+  var pass = 0;
+  var counter = 2;
+  var animationSpeed = 500;
+
+  var $colorContent = '.color-container-1 .color-content-container';
+  var $li = '.color-container-1 .main-color-container li';
+  var $liContainer = '.color-container-1 .container-';
+
+
+  $($li).on('click', function() {
+    clearInterval(colorInterval);
+    colorClick($(this));
+  });
+
+  function colorClick($this) {
+    var $getClass = $this.attr("class").split(' ');
+    var $whichNumber = $getClass[0].substr(-1);
+    var $Parent = '.color-container-1 ';
+
+    pass = 1;
+    $($colorContent).fadeOut(0);
+    $($colorContent + '-' + $whichNumber).fadeIn(animationSpeed);
+    $($li).css('border-bottom', 'none');
+    $this.css('border-bottom', '1px solid #3498db');
+
+    var reset = setTimeout(function() {
+      clearInterval(colorInterval);
+      pass = 0;
+      color1();
+    }, 7000);
+  }
+
+
+
+  var colorInterval = setInterval(function() {
+    if (pass > 0) {
+      clearInterval(colorInterval);
+      return; //stop here so that it doesn't continue to execute below code
+    }
+
+    $($colorContent).fadeOut(0);
+    $(($colorContent + '-' + counter)).fadeIn(animationSpeed);
+    $($li).css('border-bottom', 'none');
+    $($liContainer + counter).css('border-bottom', '1px solid #3498db');
+    ++counter
+
+    if (counter === $($colorContent).length + 1) {
+      counter = 1;
+    }
+
+  }, 7000);
+}
+
+
+
+
+
+
+
+color1();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkContent() {
+    var $buttons = '.whitespace-container .button';
+    var $content = '.whitespace-container .content';
+
+    $($buttons).on('click', function() {
+      var $this = $(this);
+      var $secondClass = $this.attr("class").split(' ');
+      var $thisContent = $secondClass[2] + '-' + $secondClass[3];
+      var $parent = $this.closest('.whitespace').attr('class').split(' ')[1]
+
+      $('.' + $parent + ' ' + $buttons).css('background-color', '#1A5DA1');
+      $this.css('background-color', '#003F83');
+
+      $('.' + $parent + ' ' + $content).css('display', 'none');
+      $('.' + $thisContent).fadeIn(800);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+function catchPhrase() {
+  var $allPhrase = $('.ppc-catch-phrase');
+  var $phrase = '.catch-phrase-';
+
+  var catchCounter = 2;
+  var animationSpeed = 800;
+  setInterval(function() {
+    $allPhrase.fadeOut();
+    $($phrase + catchCounter).fadeIn(animationSpeed);
+    ++catchCounter
+    if (catchCounter === $allPhrase.length + 1) {
+      catchCounter = 1;
+    }
+  }, 6000);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function smoothScroll() {
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "#") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+}
