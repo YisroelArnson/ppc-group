4e7dcffc9afeec09925de908bf974eed7eee7028
YisroelArnson <yisroelrnsn@gmail.com>
2016-07-26 15:30:38 -0400

    fix why the ppcgroup page

--- assests/js/home-page.js
+++ assests/js/home-page.js
@@ -1,11 +1,19 @@
-// $(function() {
-//   mobileNav();
-//   checkContent();
-//   catchPhrase();
-//   smoothScroll();
-//   mobileScript();
-// });
-//
+$(function() {
+  mobileNav();
+  color1();
+  checkContent();
+  catchPhrase();
+  smoothScroll();
+
+  if ( $(window).width() > 739) {
+    //Add your javascript for large screens here
+  }
+  else {
+    //Add your javascript for small screens here
+    pass = 1;
+  }
+});
+
 
 
 function mobileNav() {
@@ -20,144 +28,20 @@ function mobileNav() {
 
 
 
-// function mainSlider() {
-//   var stop1 = false;
-//   var stop2 = false;
-//   var $allLi = ' .main-parallax-container li';
-//   var $li = ' .container-';
-//   var $parallaxContentContainer = ' .parallax-content-container';
-//
-//   var counter1 = 1;
-//   var counter2 = 1;
-//   var max1 = 6;
-//   var max2 = 6;
-//   var animationSpeed = 800;
-//   var pass = 0;
-//
-//   function animateSlider1() {
-//     var $parallaxContainer1 = '.parallax-container-1';
-//     $($parallaxContainer1 + $parallaxContentContainer).hide();
-//     $($parallaxContainer1 + $parallaxContentContainer + '-' + counter1).fadeIn(animationSpeed);
-//     $($parallaxContainer1 + $allLi + ' h1').css('color', '#fff');
-//     $($parallaxContainer1 + $allLi + ' h1').css('border-bottom', 'none');
-//     // $($parallaxContainer1 + $li + counter1 + ' h1').css('color', '#666');
-//     $($parallaxContainer1 + $li + counter1 + ' h1').css('border-bottom', '3px solid #3498db');
-//     counter1 += 1;
-//
-//     if (counter1 === max1) {
-//       counter1 = 1;
-//     }
-//     callAnimate1();
-//   }
-//
-//   function animateSlider2() {
-//     var $parallaxContainer2 = '.parallax-container-2';
-//     $($parallaxContainer2 + $parallaxContentContainer).hide();
-//     $($parallaxContainer2 + $parallaxContentContainer + '-' + counter2).fadeIn(animationSpeed);
-//     $($parallaxContainer2 + $allLi + ' h1').css('border-bottom', 'none');
-//     $($parallaxContainer2 + $allLi + ' h1').css('color', '#fff');
-//     // $($parallaxContainer2 + $li + counter2 + ' h1').css('color', '#666');
-//     $($parallaxContainer2 + $li + counter2 + ' h1').css('border-bottom', '3px solid #3498db');
-//     counter2 += 1;
-//
-//     if (counter2 === max2) {
-//       counter2 = 1;
-//     }
-//     callAnimate2();
-//   }
-//
-//   function clickSlider($this) {
-//     var $getClass = $this.attr("class").split(' ');
-//     var $whichNumber = $getClass[0].substr(-1);
-//     var $thisParent = $this.closest('.parallax-container').attr('class').split(' ')[1];
-//
-//     pass = 1;
-//     $('.' + $thisParent + $allLi + ' h1').css('color', '#fff');
-//     $('.' + $thisParent + $allLi + ' h1').css('border-bottom', 'none');
-//     // $this.find('h1').css('color', '#666');
-//     $this.find('h1').css('border-bottom', '3px solid #3498db')
-//
-//     $('.' + $thisParent + $parallaxContentContainer).hide();
-//     $('.' + $thisParent + $parallaxContentContainer + '-' + $whichNumber).fadeIn(animationSpeed);
-//   }
-//
-//   $($allLi).on('click', function() {
-//     clickSlider($(this));
-//   });
-//
-//   function callAnimate1() {
-//     if (pass === 0) {
-//       var sliderInterval1 = setTimeout(animateSlider1, 5000);
-//     }
-//     else {
-//       setTimeout(function() {
-//         pass = 0;
-//         callAnimate1();
-//       }, 15000);
-//     }
-//   }
-//
-//   function callAnimate2() {
-//     if (pass === 0) {
-//       var sliderInterval2 = setTimeout(animateSlider2, 5000);
-//     }
-//     else {
-//       setTimeout(function() {
-//         pass = 0;
-//         callAnimate2();
-//       }, 15000);
-//     }
-//   }
-//
-//   callAnimate1();
-//   callAnimate2();
-// }
-//
-//
-// function mobileScript() {
-//   if ( $(window).width() > 1024) {
-//     mainSlider();
-//   }
-//   else {
-//     var $allLi = ' .main-parallax-container li';
-//     var $parallaxContentContainer = ' .parallax-content-container';
-//     function clickSlider($this) {
-//       var $getClass = $this.attr("class").split(' ');
-//       var $whichNumber = $getClass[0].substr(-1);
-//       var $thisParent = $this.closest('.parallax-container').attr('class').split(' ')[1];
-//
-//
-//       $('.' + $thisParent + $allLi + ' h1').css('color', '#fff');
-//       $this.find('h1').css('color', '#3498db');
-//
-//       $('.' + $thisParent + $parallaxContentContainer).hide();
-//       $('.' + $thisParent + $parallaxContentContainer + '-' + $whichNumber).fadeIn(800);
-//     }
-//
-//     $($allLi).on('click', function() {
-//       clickSlider($(this));
-//     });
-//   }
-// }
 
 
 
-
-
-
-
+var pass = 0;
 function color1() {
-  var pass = 0;
   var counter = 2;
   var animationSpeed = 500;
 
   var $colorContent = '.color-container-1 .color-content-container';
   var $li = '.color-container-1 .main-color-container li';
   var $liContainer = '.color-container-1 .container-';
-
+  var colorInterval;
 
   $($li).on('click', function() {
-    clearInterval(colorInterval);
     colorClick($(this));
   });
 
@@ -168,49 +52,38 @@ function color1() {
 
     pass = 1;
     $($colorContent).fadeOut(0);
-    $($colorContent + '-' + $whichNumber).fadeIn(animationSpeed);
+    $($colorContent + '-' + $whichNumber).fadeIn(animationSpeed).css('display', 'flex').css('flex-direction', 'column').css('justify-content', 'center');;
     $($li).css('border-bottom', 'none');
     $this.css('border-bottom', '1px solid #3498db');
 
     var reset = setTimeout(function() {
-      clearInterval(colorInterval);
       pass = 0;
-      color1();
+      clearInterval(colorInterval);
+      colorIntervalFun();
     }, 7000);
   }
 
 
+  function colorIntervalFun() {
+    colorInterval = setInterval(function() {
+      if (pass > 0) {
+        clearInterval(colorInterval);
+        return; //stop here so that it doesn't continue to execute below code
+      }
 
-  var colorInterval = setInterval(function() {
-    if (pass > 0) {
-      clearInterval(colorInterval);
-      return; //stop here so that it doesn't continue to execute below code
-    }
+      $($colorContent).fadeOut(0);
+      $(($colorContent + '-' + counter)).fadeIn(animationSpeed).css('display', 'flex').css('flex-direction', 'column').css('justify-content', 'center');
+      $($li).css('border-bottom', 'none');
+      $($liContainer + counter).css('border-bottom', '1px solid #3498db');
+      ++counter
 
-    $($colorContent).fadeOut(0);
-    $(($colorContent + '-' + counter)).fadeIn(animationSpeed);
-    $($li).css('border-bottom', 'none');
-    $($liContainer + counter).css('border-bottom', '1px solid #3498db');
-    ++counter
-
-    if (counter === $($colorContent).length + 1) {
-      counter = 1;
-    }
-
-  }, 7000);
+      if (counter === $($colorContent).length + 1) {
+        counter = 1;
+      }
+    }, 10000);
+  }
+  colorIntervalFun();
 }
-
-
-
-
-
-
-
-color1();
-
-
-
-
 
 
 
@@ -226,7 +99,9 @@ color1();
 function checkContent() {
     var $buttons = '.whitespace-container .button';
     var $content = '.whitespace-container .content';
+    var $gif = '.whitespace-gif';
 
+    var animationSpeed = 600;
     $($buttons).on('click', function() {
       var $this = $(this);
       var $secondClass = $this.attr("class").split(' ');
@@ -236,13 +111,12 @@ function checkContent() {
       $('.' + $parent + ' ' + $buttons).css('background-color', '#1A5DA1');
       $this.css('background-color', '#003F83');
 
+      $('.' + $parent + ' ' + $gif).fadeOut(animationSpeed);
+      $('.' + $parent + ' ' + $gif + '-' + $secondClass[3]).fadeIn(animationSpeed);
       $('.' + $parent + ' ' + $content).css('display', 'none');
-      $('.' + $thisContent).fadeIn(800);
+      $('.' + $thisContent).fadeIn(animationSpeed);
     });
 }
-
-
-
 
 
 
@@ -279,17 +153,8 @@ function catchPhrase() {
 
 
 
-
-
-
-
-
-
-
-
-
 function smoothScroll() {
-  $("a").on('click', function(event) {
+  $("a").not('.no-scroll').on('click', function(event) {
 
     if (this.hash !== "#") {
       event.preventDefault();
