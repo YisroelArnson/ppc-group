4eabfa7b81719115bd0a07a000e0939689e98a0f
YisroelArnson <yisroelrnsn@gmail.com>
2016-08-08 19:24:19 -0400

    made all sections change by them selves

--- assests/js/home-page.js
+++ assests/js/home-page.js
@@ -126,16 +126,28 @@ function color1() {
 
 
 
-
 function checkContent() {
     var $buttons = '.whitespace-container .button';
     var $content = '.whitespace-container .content';
     var $gif = '.whitespace-gif';
 
+    var changePass1 = 0;
+    var changePass2 = 0;
+    var changePass3 = 0;
+    var timeBetween = 5000;
     var animationSpeed = 600;
+    var contentInterval1;
+    var contentInterval2;
+    var contentInterval3;
+    var counter1 = 2;
+    var counter2 = 7;
+    var counter3 = 11;
 
     $($buttons).on('click', function() {
-      var $this = $(this);
+      changeButtons($(this));
+    });
+
+    function changeButtons($this) {
       var $secondClass = $this.attr("class").split(' ');
       var $thisContent = $secondClass[2] + '-' + $secondClass[3];
       var $parent = $this.closest('.whitespace').attr('class').split(' ')[1]
@@ -147,7 +159,114 @@ function checkContent() {
       $('.' + $parent + ' ' + $gif + '-' + $secondClass[3]).fadeIn(animationSpeed);
       $('.' + $parent + ' ' + $content).css('display', 'none');
       $('.' + $thisContent).fadeIn(animationSpeed);
-    });
+
+      if ($parent === 'whitespace-1') {
+        changePass1 = 1;
+        var reset = setTimeout(function() {
+          changePass1 = 0;
+          clearInterval(contentInterval1);
+          checkContentInterval1();
+        }, 7000);
+      }
+
+      else if ($parent === 'whitespace-2') {
+        changePass2 = 1;
+        var reset = setTimeout(function() {
+          changePass2 = 0;
+          clearInterval(contentInterval2);
+          checkContentInterval2();
+        }, 7000);
+      }
+
+      else if ($parent === 'whitespace-3') {
+        changePass3 = 1;
+        var reset = setTimeout(function() {
+          changePass3 = 0;
+          clearInterval(contentInterval3);
+          checkContentInterval3();
+        }, 7000);
+      }
+    }
+
+    function checkContentInterval1() {
+
+      contentInterval1 = setInterval(function() {
+        var $parent = '.whitespace-1 ';
+        if (changePass1 > 0) {
+          clearInterval(contentInterval1);
+          return;
+        }
+
+        $($parent + $buttons).css('background-color', '#1A5DA1');
+        $($parent + $buttons + '-' + counter1).css('background-color', '#003F83');
+
+        $($parent + $gif).fadeOut(animationSpeed);
+        $($parent + $gif + '-' + counter1).fadeIn(animationSpeed);
+
+        $($parent + $content).css('display', 'none');
+        $($parent + $content + '-container-' + counter1).fadeIn(animationSpeed);
+        ++counter1;
+
+        if (counter1 === $($parent + $buttons).length) {
+          counter1 = 1;
+        }
+
+      }, timeBetween);
+    }
+
+    function checkContentInterval2() {
+      contentInterval2 = setInterval(function() {
+        var $parent = '.whitespace-2 ';
+        if (changePass2 > 0) {
+          clearInterval(contentInterval1);
+          return;
+        }
+
+        $($parent + $buttons).css('background-color', '#1A5DA1');
+        $($parent + $buttons + '-' + counter2).css('background-color', '#003F83');
+
+        $($parent + $gif).fadeOut(animationSpeed);
+        $($parent + $gif + '-' + counter2).fadeIn(animationSpeed);
+
+        $($parent + $content).css('display', 'none');
+        $($parent + $content + '-container-' + counter2).fadeIn(animationSpeed);
+        ++counter2;
+
+        if (counter2 === 10) {
+          counter2 = 6;
+        }
+      }, timeBetween);
+    }
+
+    function checkContentInterval3() {
+      contentInterval3 = setInterval(function() {
+        var $parent = '.whitespace-3 ';
+        if (changePass3 > 0) {
+          clearInterval(contentInterval1);
+          return;
+        }
+
+        $($parent + $buttons).css('background-color', '#1A5DA1');
+        $($parent + $buttons + '-' + counter3).css('background-color', '#003F83');
+
+        $($parent + $gif).fadeOut(animationSpeed);
+        $($parent + $gif + '-' + counter3).fadeIn(animationSpeed);
+
+        $($parent + $content).css('display', 'none');
+        $($parent + $content + '-container-' + counter3).fadeIn(animationSpeed);
+        ++counter3;
+
+        if (counter3 === 15) {
+          counter3 = 10;
+        }
+      }, timeBetween);
+    }
+
+
+
+  checkContentInterval1();
+  checkContentInterval2();
+  checkContentInterval3();
 }
 
 
